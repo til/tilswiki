dd2451f84cd6b550af2dd10bca57c11dbbffe328
Tilmann Singer <tils@tils.net>
2009-03-26 00:46:58 +0100

    Update gems to merb 1.0.10, include merb-more

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,10 +1,11 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.9"
+merb_gems_version = "1.0.10"
 dm_gems_version   = "0.9.10"
 do_gems_version   = "0.9.11"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-core", merb_gems_version 
+dependency "merb-more", merb_gems_version 
 dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version  
 dependency("merb-cache", merb_gems_version) do
