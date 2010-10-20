0ee69d17b829eda506378430a0007be8ce27feb6
Tilmann Singer <tils@tils.net>
2010-10-20 14:51:21 +0200

    Create new rails
    rails new . --skip-active-record --skip-prototype --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tilswiki::Application.config.secret_token = 'f2fbab5324b5d6204235e30acfa64f65d8cd0f3ef18b59233556c372b91a4bee8d04f61d5d4d4d9738e3f642cf58430bd5ccd71e67cf67b2fd690b5bb621f189'
